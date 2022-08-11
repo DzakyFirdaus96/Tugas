3c1bb446a0b819ef7f5e568ad332a7e846324964
DzakyFirdaus96 <110155168+DzakyFirdaus96@users.noreply.github.com>
2022-08-11 17:01:09 +0700

    Delete biodatadb.sql

--- biodatadb.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `biodata` (
-  `nama` varchar(30) NOT NULL,
-  `kelas` varchar(30) DEFAULT NULL,
-  `jurusan` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`nama`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
